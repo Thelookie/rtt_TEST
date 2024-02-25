3b1f8bd2edd5dc5ed5e7e099271602e2737c2890
Ubuntu <ubuntu@ip-172-31-21-125.ec2.internal>
2024-02-25 13:21:04 +0000

    modify client part

--- client_rtt_test.py
+++ client_rtt_test.py
@@ -1,48 +1,47 @@
 import socket
-import random
-import string
 
 SERVER_IP = '166.104.246.42'
-SERVER_PORT = 8000
-RECV_WINDOW_SIZE = 4096
+SERVER_PORT = 12400
+RECV_WINDOW_SIZE = 4  # 수신 윈도우 크기를 조정합니다.
 
 def set_congestion_control_algorithm(sock):
-    # Set TCP congestion control algorithm to Cubic
+    # TCP 혼잡 제어 알고리즘을 Cubic으로 설정
     sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, b'cubic')
 
 def set_receive_window_size(sock, size):
-    # Set receive window size
+    # 수신 윈도우 크기 설정
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, size)
 
 def get_random_data(sock):
-    # Send GET request
+    # GET 요청 보내기
     sock.sendall(b'GET /random HTTP/1.1\r\nHost: localhost\r\n\r\n')
 
-    # Receive response
+    # 응답 받기
     response = b''
     while True:
-        data = sock.recv(4096)
+        # 지정된 윈도우 크기로 데이터 받기
+        data = sock.recv((RECV_WINDOW_SIZE*1024))
         if not data:
             break
         response += data
 
     return response
 
-# Create a TCP socket
+# TCP 소켓 생성
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    # Set congestion control algorithm
+    # 혼잡 제어 알고리즘 설정
     set_congestion_control_algorithm(sock)
 
-    # Set receive window size
+    # 수신 윈도우 크기 설정
     set_receive_window_size(sock, RECV_WINDOW_SIZE)
 
-    # Connect to the server
+    # 서버에 연결
     sock.connect((SERVER_IP, SERVER_PORT))
 
-    # Get random data from the server
+    # 서버로부터 무작위 데이터 가져오기
     random_data = get_random_data(sock)
 
-# Print the received random data
-print("Received random data:")
+# 받은 무작위 데이터 출력
+print("수신한 무작위 데이터:")
 print(random_data.decode('utf-8'))
 
